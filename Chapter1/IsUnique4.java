f38589e78785d96046d51c1df1d924c80443ef11
Pratyush Patwa <pratyush.patwa@gmail.com>
2020-10-21 15:46:46 +0530

    Add files via upload

---
+++ Chapter1/IsUnique4.java
@@ -0,0 +1,26 @@
+package Chapter1;
+import java.util.Scanner;
+public class IsUnique4 {
+	public static void main(String[]args){
+    	Scanner sc = new Scanner(System.in);
+    	String str = sc.nextLine();
+    	str = str.toLowerCase();
+    	System.out.println(checkUnique(str));
+    	sc.close();
+   
+    }    	
+    public static boolean checkUnique(String str){
+    	int checker =0;
+    	
+   
+    	for(int i=0; i<str.length();i++){
+		int index = str.charAt(i)-'a';
+		
+    		if ((checker & (1<< index))>0)
+    			return false;
+    		
+    		checker  |= (1<<index);
+    	}
+    	return true;
+    }
+}
